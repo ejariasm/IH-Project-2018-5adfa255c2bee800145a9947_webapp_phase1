71b168b0ee6d815b3b084363e46ce1e823746035
dscastillol <dscastillol@unal.edu.co>
2018-05-09 21:33:35 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## webapp_phase1
 
-undefined
+# NY District Finder
+## Keywords
+
+ - Security
+ - Location
+ - Efficiency
+ 
+ ## Datasets
+ 
+ - [Neighborhood Names GIS] [https://catalog.data.gov/dataset/neighborhood-names-gis][the_geom][299]
+ ## Brief Description
+ My map is centered in the NY Stern School of Bussines. The page have a button that provide information of the dataset used and put it on a table.
+## Browsers
+Chrome,Edge and Mozilla.
+
--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+const BOOUGHT_NY_URL= ("https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD");
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+var map;
+
+var BOROUGHT_NY=[];
+
+function getDataFromURL(URL) {
+    var data = $.get(BOOUGHT_NY_URL, function () {
+        var posLen = data.responseJSON.data.length;
+        for (i=0; i<posLen;i++) {
+            var posicion = data.responseJSON.data[i][9];
+            console.log(posicion);
+            BOROUGHT_NY.push(posicion);
+        }
+    })
+}
+
+function updateTable() {
+    var tableRef = document.getElementById('posTable').getElementsByTagName('tbody')[0];
+    for (j=0; j<BOROUGHT_NY.length;j++){
+        // Insert a row in the table at the last row
+        var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+// Insert a cell in the row at index 0
+        var newCell  = newRow.insertCell(0);
+
+// Append a text node to the cell
+        var newText  = document.createTextNode(BOROUGHT_NY[j]);
+
+        newCell.appendChild(newText);
+    }
+
+
+}
+
+
+function initMap() {
+    var myLatLng = {lat: 40.7290549, lng: -73.9965233};
+    map = new google.maps.Map(document.getElementById('googleMapContainer'), {
+        center : myLatLng,
+        zoom: 18
     });
-})();
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'NYU Stern School of Business'
+    });
+}
